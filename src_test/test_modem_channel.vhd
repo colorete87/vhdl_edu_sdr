d2c9b81b78f11870913824e608f4f35e729b715e
colorete87 <federico.zacchigna@gmail.com>
2020-09-30 04:27:01 -0300

    Sources for tests updated

--- src_test/test_modem_channel.vhd
+++ src_test/test_modem_channel.vhd
@@ -63,7 +63,7 @@ architecture rtl of test_modem_channel is
       send_i           : in  std_logic;
       -- State      
       tx_rdy_o         : out std_logic;
-      rx_rdy_o         : out std_logic
+      rx_ovf_o         : out std_logic
     );
   end component bb_modem;
 
@@ -100,17 +100,19 @@ architecture rtl of test_modem_channel is
   signal send_s         : std_logic;
   -- Modem State        : std_logic;
   signal tx_rdy_s       : std_logic;
-  signal rx_rdy_s       : std_logic;
+  signal rx_ovf_s       : std_logic;
 
   -- Modem config
   constant nm1_bytes_c  : std_logic_vector( 7 downto 0) := X"03";
   constant nm1_pre_c    : std_logic_vector( 7 downto 0) := X"07";
   constant nm1_sfd_c    : std_logic_vector( 7 downto 0) := X"03";
-  constant det_th_c     : std_logic_vector(15 downto 0) := X"0400";
-  constant pll_kp_c     : std_logic_vector(15 downto 0) := X"0400";
-  constant pll_ki_c     : std_logic_vector(15 downto 0) := X"0100";
+  constant det_th_c     : std_logic_vector(15 downto 0) := X"0040";
+  constant pll_kp_c     : std_logic_vector(15 downto 0) := X"A000";
+  constant pll_ki_c     : std_logic_vector(15 downto 0) := X"9000";
   -- Channel config
   constant sigma_c      : std_logic_vector(15 downto 0) := X"0000"; -- QU16.12
+
+  signal counter_s : integer;
 
 begin
 
@@ -119,29 +121,48 @@ begin
 
   -- Modem Control
   u_modem_control : process(clk_i)
-    variable counter_v : integer;
-    constant VAL_L     : integer := 1234;
-    constant VAL_H     : integer := 13;
+    variable counter_v  : integer;
+    variable send_val_v : integer := 123;
+    constant VAL_L      : integer := 1242;
+    constant VAL_H      : integer := 13;
   begin
     if rising_edge(clk_i) then
       if srst_i = '1' then
-        counter_v := VAL_L-32;
+        -- counter_v := VAL_L-32;
+        counter_v := 0;
         send_s <= '0';
       else
         if en_i = '1' then
-          counter_v := counter_v + 1;
-          if send_s = '0' and counter_v = VAL_L then
-            if tx_rdy_s = '1' then
+          if tx_rdy_s = '1' then
+            counter_v := counter_v + 1;
+            if counter_v = send_val_v then
               send_s <= '1';
+              send_val_v := send_val_v + 67;
+              counter_v := 0;
             end if;
-            counter_v := 0;
           end if;
-          if send_s = '1' and counter_v = VAL_H then
-            send_s <= '0';
-            counter_v := 0;
+          if send_s = '1' then
+            if counter_v = VAL_H then
+              send_s <= '0';
+              counter_v := 0;
+            else
+              counter_v := counter_v + 1;
+            end if;
           end if;
+          -- counter_v := counter_v + 1;
+          -- if send_s = '0' and counter_v = VAL_L then
+          --   if tx_rdy_s = '1' then
+          --     send_s <= '1';
+          --   end if;
+          --   counter_v := 0;
+          -- end if;
+          -- if send_s = '1' and counter_v = VAL_H then
+          --   send_s <= '0';
+          --   counter_v := 0;
+          -- end if;
         end if;
       end if;
+      counter_s <= counter_v;
     end if;
   end process;
 
@@ -180,7 +201,7 @@ begin
     send_i        => send_s,
     -- State      
     tx_rdy_o      => tx_rdy_s,
-    rx_rdy_o      => rx_rdy_s
+    rx_ovf_o      => rx_ovf_s
   );
 
   -- Channel
