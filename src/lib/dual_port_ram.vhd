f9e04189a706ac33d1e25b58775963cf9801689f
colorete87 <federico.zacchigna@gmail.com>
2020-10-07 03:28:19 -0300

    tabs removed

--- src/lib/dual_port_ram.vhd
+++ src/lib/dual_port_ram.vhd
@@ -7,23 +7,23 @@ entity dual_port_ram is
     SYNC_READ : boolean := true
   );
   port (
-    Wr_clock : in std_ulogic;
-    We       : in std_ulogic; -- Write enable
+    Wr_clock : in std_logic;
+    We       : in std_logic; -- Write enable
     Wr_addr  : in natural range 0 to MEM_SIZE-1;
-    Wr_data  : in std_ulogic_vector;
+    Wr_data  : in std_logic_vector;
 
-    Rd_clock : in std_ulogic;
-    Re       : in std_ulogic; -- Read enable
+    Rd_clock : in std_logic;
+    Re       : in std_logic; -- Read enable
     Rd_addr  : in natural range 0 to MEM_SIZE-1;
-    Rd_data  : out std_ulogic_vector
+    Rd_data  : out std_logic_vector
   );
 end entity;
 
 architecture rtl of dual_port_ram is
-  type ram_type is array (0 to MEM_SIZE-1) of std_ulogic_vector(Wr_data'length-1 downto 0);
+  type ram_type is array (0 to MEM_SIZE-1) of std_logic_vector(Wr_data'length-1 downto 0);
   signal ram : ram_type;
 
-  signal sync_rdata : std_ulogic_vector(Rd_data'range);
+  signal sync_rdata : std_logic_vector(Rd_data'range);
 begin
   assert Wr_data'length = Rd_data'length report "Data bus size mismatch" severity failure;
 
